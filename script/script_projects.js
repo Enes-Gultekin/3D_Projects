319e83478ea70ebff567a5a981b13dcdbd15bb72
malat <m.enes.gultkn@gmail.com>
2026-01-08 22:22:10 +0100

    map adjustments

--- script/script_projects.js
+++ script/script_projects.js
@@ -5,13 +5,15 @@ const myData = [
     name: "kenya",
     url: "../data/kenya_border.geojson",
     color: "red",
+    ISO_A2: "KE",
   },
   {
-    lat: 40.59574107567681,
-    lng: -5.335875699829245,
+    lat: -22.392482957234137,
+    lng: 24.175533848483212,
     name: "bostwana",
     url: "../data/bostwana_border.geojson",
     color: "cyan",
+    ISO_A2: "BW",
   },
   {
     lat: 49.75144630859725,
@@ -19,6 +21,7 @@ const myData = [
     name: "trier",
     url: "../data/germany_border.geojson",
     color: "yellow",
+    ISO_A2: "DE",
   },
   {
     lat: 51.521980853860214,
@@ -26,37 +29,36 @@ const myData = [
     name: "london",
     url: "../data/britain_border.geojson",
     color: "white",
+    ISO_A2: "GB",
   },
   {
     lat: 50.521980853860214,
     lng: 10.491358329269445,
     name: "germany",
     url: "../data/germany_border.geojson",
-    color: "white",
+    color: "blue",
+    ISO_A2: "DE",
+  },
+  {
+    lat: 41.02365621207687,
+    lng: -3.596563756253431,
+    name: "spain",
+    url: "../data/spain_border.geojson",
+    color: "yellow",
+    ISO_A2: "ES",
   },
 ];
 
-// let kenya_border = [];
-// Papa.parse("../data/kenya_border.csv", {
-//   download: true,
-//   header: false,
-//   step: function (row) {
-//     kenya_border.push(row.data.reverse());
+let kenya_border = [];
 
-//   },
-
-//   complete: function () {
-//     console.log("kenya_border is added");
-//   },
-// });
-
-// const p_card = document.getElementById("projects_card");
-// const close_button = document.getElementById("close_button");
+const p_card = document.getElementById("projects_card");
+const close_button = document.getElementById("close_button");
 // const world = Globe()(document.getElementById("globeViz"))
 //   .globeImageUrl("img/earth.jpg")
-//    .backgroundImageUrl('//cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png')
-//   .polygonsData(kenya_border)
-//   .polygonStrokeColor("red")
+//   .backgroundImageUrl(
+//     "//cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png"
+//   )
+
 //   .htmlElementsData(myData)
 //   .htmlElement((d) => {
 //     const el = document.createElement("div");
@@ -83,83 +85,103 @@ const myData = [
 //     return el;
 //   });
 
+// const colorScale = scaleSequentialSqrt(interpolateYlOrRd)
+// // GDP per capita (avoiding countries with small pop)
+// const getVal = feat => feat.properties.GDP_MD_EST / Math.max(1e5, feat.properties.POP_EST);
+
+// fetch('https://raw.githubusercontent.com/vasturiano/globe.gl/master/example/datasets/ne_110m_admin_0_countries.geojson').then(res => res.json()).then(countries =>
+// {
+//   const maxVal = Math.max(...countries.features.map(getVal));
+
+//   const world = new Globe(document.getElementById('globeViz'))
+//     .globeImageUrl('//cdn.jsdelivr.net/npm/three-globe/example/img/earth-night.jpg')
+//     .backgroundImageUrl('//cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png')
+//     .lineHoverPrecision(0)
+//     .polygonsData(countries.features.filter(d => d.properties.ISO_A2 !== 'AQ'))
+//     .polygonAltitude(0.06)
+
+//     .polygonSideColor(() => 'rgba(0, 100, 0, 0.15)')
+//     .polygonStrokeColor(() => '#111')
+//     .polygonLabel(({ properties: d }) => `
+//       <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
+//       GDP: <i>${d.GDP_MD_EST}</i> M$<br/>
+//       Population: <i>${d.POP_EST}</i>
+//     `)
+//     .onPolygonHover(hoverD => world
+//       .polygonAltitude(d => d === hoverD ? 0.12 : 0.06)
+//       .polygonCapColor(d => d === hoverD ? 'steelblue' : colorScale(getVal(d)))
+//     )
+//     .polygonsTransitionDuration(300);
+// });
+
+//fetch countries borders
+fetch("../data/countries.geojson")
+  .then((res) => res.json())
+  .then((countries) => {
+    const world = new Globe(document.getElementById("globeViz"))
+      .globeImageUrl(
+        "//cdn.jsdelivr.net/npm/three-globe/example/img/earth-night.jpg"
+      )
+      .backgroundImageUrl(
+        "//cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png"
+      )
+      .lineHoverPrecision(0)
+      .polygonsData(
+        countries.features.filter((d) =>
+          myData.some((item) => item.ISO_A2 === d.properties.ISO_A2)
+        )
+      )
+      .polygonAltitude(0.06)
+      .showGraticules(true)
+      .polygonSideColor(() => "rgba(0, 100, 0, 0.15)")
+      .polygonStrokeColor(() => "#111")
+      .polygonLabel(
+        ({ properties: d }) => `
+          <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
+          GDP: <i>${d.GDP_MD_EST}</i> M$<br/>
+          Population: <i>${d.POP_EST}</i>
+        `
+      )
+      .onPolygonHover((hoverD) =>
+        world.polygonAltitude((d) => (d === hoverD ? 0.12 : 0.06))
+      )
+      .polygonsTransitionDuration(300)
+      .htmlElementsData(myData)
+      .htmlElement((d) => {
+        const el = document.createElement("div");
+        el.className = "marker-wrapper";
+
+        el.innerHTML = `
+        <div class="pulse" style="border-color: ${d.color}">${d.name}</div>
+
+      `;
+
+        el.onmouseover = () => console.log("Hovering over " + d.name);
+        el.onclick = () => {
+          world.controls().autoRotate = false;
+          p_card.style.right = "200px";
+          document.getElementById("projects_card").classList.add("active");
+          document.getElementById("card_title").innerText = d.name;
+          document.getElementById(
+            "card_description"
+          ).innerText = `This is my work in ${d.name}.`;
+          world.pointOfView({ lat: d.lat, lng: d.lng, altitude: 0.5 }, 1000);
+        };
+
+        //dialog window
+
+        document.getElementById("close_button").onclick = () => {
+          p_card.style.right = "0px";
+          document.getElementById("projects_card").classList.remove("active");
+          world.pointOfView({ altitude: 2.5 }, 1000);
+          world.controls().autoRotate = true;
+          world.controls().autoRotateSpeed = 0.2;
+        };
+
+        return el;
+      });
+  });
+
 // world.controls().autoRotate = true;
-// world.controls().autoRotateSpeed = 0.2;
-// document.getElementById("close_button").onclick = () => {
-//   p_card.style.right = "0px";
-//   // Hide the card
-//   document.getElementById("projects_card").classList.remove("active");
 
-//   // Zoom back out to show the whole world
-//   world.pointOfView({ altitude: 2.5 }, 1000);
-
-//   // Start spinning again
-//   world.controls().autoRotate = true;
-// };
-const kenya = {
-  type: "FeatureCollection",
-  features: [
-    {
-      type: "Feature",
-      properties: {},
-      geometry: {
-        coordinates: [
-          [
-            [25.087396898831884, 19.893829895727322],
-            [25.087396898831884, 11.827486622238752],
-            [33.1432490853297, 11.827486622238752],
-            [33.1432490853297, 19.893829895727322],
-            [25.087396898831884, 19.893829895727322],
-          ],
-        ],
-        type: "Polygon",
-      },
-    },
-  ],
-};
-
-// fetch("../data/kenya_protected.geojson")
-//   .then((res) => res.json())
-//   .then((kenyapr_json) => {
-//     console.log(kenyapr_json.features);
-//     const world = Globe()(document.getElementById("globeViz"))
-//   .globeImageUrl("img/earth.jpg")
-//   .polygonsData(kenyapr_json.features);
-
-//   });
-
-// fetch("../data/kenya_border.geojson")
-//   .then((res) => res.json())
-//   .then((kenyapr_json) => {
-//     console.log(kenyapr_json.features);
-//     const world = Globe()(document.getElementById("globeViz"))
-//   .globeImageUrl("img/earth.jpg")
-//   .polygonsData(kenyapr_json.features)
-//   .polygonCapColor((0,0,0,0))
-
-//   });
-const world = Globe()(document.getElementById("globeViz"))
-  .globeImageUrl("img/earth.jpg")
-  .polygonAltitude(0.06);
-async function fetchAllGeoJson(myData) {
-  for (let i = 0; i < myData.length; i++) {
-    try {
-      console.log(`Fetching: ${myData[i].url}`);
-
-      const response = await fetch(myData[i].url);
-      if (!response.ok)
-        throw new Error(`HTTP error! status: ${response.status}`);
-
-      const data = await response.json();
-      console.log(data.features);
-      world.polygonsData(data.features);
-
-      //
-
-      // You can store the results in an array or add them to a map here
-    } catch (error) {
-      console.error(`Could not fetch data for index ${i}:`, error);
-    }
-  }
-}
-fetchAllGeoJson(myData);
+//close button attributes
